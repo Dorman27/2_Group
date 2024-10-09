17fb08e2524ac8fe4a174133cee764a2b00299ab
Dorman27 <54972763+Dorman27@users.noreply.github.com>
2024-10-09 20:06:06 +0900

    Update GamblingGame.cpp

--- Homework/hw#2/GamblingGame.cpp
+++ Homework/hw#2/GamblingGame.cpp
@@ -1,32 +1,57 @@
 #include "GamblingGame.h"
+#include <cstdlib>
+#include <ctime>
 
-void GamblingGame::play() {
-    srand((unsigned)time(0));  
-    while (true) {
-        if (takeTurn(player1)) break;
-        if (takeTurn(player2)) break; 
-    }
+GamblingGame::~GamblingGame() 
+{
+    std::cout << "게임을 종료합니다." << std::endl;
 }
 
-bool GamblingGame::takeTurn(Player* player) {
-    cout << player->getName() << " <Enter> 키 입력" << flush;
+void GamblingGame::play() 
+{
+    std::string p1, p2;
+    std::cout << "***** 겜블링 게임을 시작합니다. *****" << std::endl;
+    std::cout << "첫번째 선수 이름>>";
+    std::cin >> p1;
+    p[0].setName(p1);
+    std::cout << "두번째 선수 이름>>";
+    std::cin >> p2;
+    p[1].setName(p2);
+    std::cin.ignore();
+    srand((unsigned)time(0));
+    int end = 0;
 
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    while (true)
+    {
+        for (int k = 0; k < 2; k++) 
+        {
+            std::cout << p[k].getName() << ":<Enter>" << std::endl;
+            while (true)
+            {
+                char ch;
+                std::cin.get(ch);
+                if (ch == '\n')
+                    break;
+            }
+            
+            int n[3];
+            std::cout << "\t" << "\t";
 
-    cin.get();
-
-    int num1 = rand() % 3;
-    int num2 = rand() % 3;
-    int num3 = rand() % 3;
-
-    cout << num1 << " " << num2 << " " << num3 << " ";
-
-    if (num1 == num2 && num2 == num3) {
-        cout << player->getName() << "님 승리!!" << endl;
-        return true;
-    }
-    else {
-        cout << "아쉽군요!" << endl;
-        return false;
+            for (int i = 0; i < 3; i++)
+            {
+                n[i] = rand() % 3;
+                std::cout << n[i] << "\t";
+            }
+            if (n[0] == n[1] && n[0] == n[2] && n[1] == n[2])
+            {
+                std::cout << p[k].getName() << "님 승리!!" << std::endl;
+                end = 1;
+                break;
+            }
+            else
+                std::cout << "아쉽군요!" << std::endl;
+        }
+        if (end == 1)
+            break;
     }
 }
