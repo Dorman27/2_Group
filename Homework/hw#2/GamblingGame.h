c7889e2a246d0166e32df820aed3da7f5a575a48
Dorman27 <54972763+Dorman27@users.noreply.github.com>
2024-10-09 20:06:32 +0900

    Update GamblingGame.h

--- Homework/hw#2/GamblingGame.h
+++ Homework/hw#2/GamblingGame.h
@@ -1,24 +1,15 @@
-#pragma once
 #ifndef GAMBLINGGAME_H
 #define GAMBLINGGAME_H
 
 #include "Player.h"
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
 
-class GamblingGame {
-    Player* player1;
-    Player* player2;
-
+class GamblingGame
+{
+    Player p[2];
 public:
-    GamblingGame(Player* p1, Player* p2) : player1(p1), player2(p2) {}
-
     void play();
-
-private:
-    bool takeTurn(Player* player);
+    ~GamblingGame();
 };
 
-#endif
+#endif 
